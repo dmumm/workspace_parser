14ed28b6b9339d5180df1ffdd2f83e51c47eb9e0
dmumm <32206975+dmumm@users.noreply.github.com>
2023-10-27 16:58:13 -0400

    sort, check all directories, copy cell by M1

--- workspace_parser.py
+++ workspace_parser.py
@@ -2,10 +2,27 @@ import json
 import tkinter as tk
 from tkinter import ttk
 import os
+from datetime import datetime
+
+# Convert Unix timestamp to human-readable date-time
+def convert_timestamp_to_date(timestamp):
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
 # Sorting function
 def treeview_sort_column(tv, col, reverse):
-    l = [(tv.set(k, col), k) for k in tv.get_children('')]
+    # Fetch a sample item to determine the type of the data in the column
+    sample_item = tv.set(tv.get_children('')[0], col)
+
+    # Check if the sample item is numeric
+    is_numeric = sample_item.replace('.', '', 1).isdigit()
+
+    if is_numeric:
+        # Sort as numbers
+        l = [(float(tv.set(k, col)), k) for k in tv.get_children('')]
+    else:
+        # Sort as strings (case-insensitive)
+        l = [(tv.set(k, col).lower(), k) for k in tv.get_children('')]
+
     l.sort(reverse=reverse)
 
     # rearrange items in sorted positions
@@ -15,26 +32,46 @@ def treeview_sort_column(tv, col, reverse):
     # reverse sort next time
     tv.heading(col, command=lambda: treeview_sort_column(tv, col, not reverse))
 
-# Determine the path to the WorkspaceCache file
-workspace_cache_path = os.path.join(os.environ['LOCALAPPDATA'], 'Microsoft', 'Edge Dev', 'User Data', 'Default', 'Workspaces', 'WorkspacesCache')
+# Determine the path to the WorkspaceCache files
+paths_to_check = [
+    os.path.join(os.environ['LOCALAPPDATA'], 'Microsoft', 'Edge Dev', 'User Data', 'Default', 'Workspaces', 'WorkspacesCache'),
+    os.path.join(os.environ['LOCALAPPDATA'], 'Microsoft', 'Edge Canary', 'User Data', 'Default', 'Workspaces', 'WorkspacesCache'),
+    os.path.join(os.environ['LOCALAPPDATA'], 'Microsoft', 'Edge', 'User Data', 'Default', 'Workspaces', 'WorkspacesCache')
+]
 
-# Read the JSON data from the file
-with open(workspace_cache_path, 'r', encoding='utf-8') as file:
-    parsed_data = json.load(file)
+# Dictionary to hold final workspaces after checking duplicates and last active time
+workspace_dict = {}
 
-workspaces = parsed_data['workspaces']
+# Parsing multiple paths
+for path in paths_to_check:
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as file:
+            parsed_data = json.load(file)
+
+        workspaces = parsed_data['workspaces']
+
+        for workspace in workspaces:
+            name = workspace['name']
+
+            # Check for duplicates and replace if newer
+            if name not in workspace_dict or (workspace.get('last_active_time', 0) > workspace_dict[name].get('last_active_time', 0)):
+                workspace_dict[name] = workspace
+
+# Convert the dictionary to a list for iteration
+final_workspaces = list(workspace_dict.values())
+
 
 # Create main window
 root = tk.Tk()
 root.title("Workspaces Table")
 
 # Create treeview for table representation
-tree = ttk.Treeview(root, columns=('Name', 'Count', 'ID', 'Connection URL'), show='headings')
+tree = ttk.Treeview(root, columns=('Name', 'Count', 'ID', 'Last Active Time', 'Connection URL'), show='headings')
 
 tree.heading('Name', text='Workspace Name', command=lambda: treeview_sort_column(tree, 'Name', False))
 tree.heading('Count', text='Tab Count', command=lambda: treeview_sort_column(tree, 'Count', False))
 tree.heading('ID', text='ID', command=lambda: treeview_sort_column(tree, 'ID', False))
-
+tree.heading('Last Active Time', text='Last Active Time', command=lambda: treeview_sort_column(tree, 'Last Active Time', False))
 tree.heading('Connection URL', text='Connection URL', command=lambda: treeview_sort_column(tree, 'Connection URL', False))
 
 tree.pack(fill=tk.BOTH, expand=1)
@@ -51,9 +88,20 @@ color_map = {
     # ... add other mappings as needed
 }
 
-for workspace in workspaces:
-    tree.insert('', 'end', values=(workspace['name'], workspace['count'], workspace['id'], workspace['connectionUrl']),
-            tags=(color_map.get(workspace['color'], "#FFFFFF"),))  # Default to white if color not found in map
+# Populate treeview with final workspaces
+for workspace in final_workspaces:
+    name = workspace['name']
+    count = workspace['count']
+    id_val = workspace['id']
+    connection_url = workspace['connectionUrl']
+    # Using the get method allows for a default value if the key doesn't exist
+    last_active_time = workspace.get('last_active_time', "")
+    if last_active_time:  # if timestamp exists
+        last_active_time = convert_timestamp_to_date(float(last_active_time))
+
+    tree.insert('', 'end', values=(name, count, id_val, last_active_time, connection_url),
+                tags=(color_map.get(workspace['color'], "#FFFFFF"),))
+
 
 for color, hex_value in color_map.items():
     tree.tag_configure(hex_value, background=hex_value)
